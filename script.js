79a17697a263edfd3aa331e7043db87ecdf344c4
zahid072 <jahidhasan19368@gmail.com>
2024-02-27 22:18:44 +0600

    UI complete

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const loadPhone = async () => {
+  const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+  let datas = await res.json();
+  displayPhone(datas.data)
+  
+} 
+loadPhone()
 
+const displayPhone = (datas)=>{
+  for(let data of datas){
+    console.log(data)
+  }
+
+}
