fde15e67d1bace7250fbf68a49192491ecec044a
zahid072 <jahidhasan19368@gmail.com>
2024-02-27 22:50:53 +0600

    add card info

--- script.js
+++ script.js
@@ -7,8 +7,27 @@ const loadPhone = async () => {
 loadPhone()
 
 const displayPhone = (datas)=>{
+  const container = document.getElementById('allCard');
   for(let data of datas){
-    console.log(data)
+   let cardContainer = document.createElement('div');
+   cardContainer.classList.add('card', 'w-96', 'bg-base-100', 'shadow-xl');
+   cardContainer.innerHTML = `
+         <!-- card -->
+         
+            <figure class="px-10 pt-10">
+              <img src=${data.image} alt="Shoes" class="rounded-xl" />
+            </figure>
+            <div class="card-body items-center text-center">
+              <h2 class="card-title">${data.phone_name}</h2>
+              <p>There are many variations of passages of available, but the majority have suffered</p>
+              <h1 class="font-bold">$999</h1>
+              <div class="card-actions">
+                <button onclick="seeDetails.showModal()" class="btn btn-primary">See Details</button>
+              </div>
+            </div>
+          
+   `
+   container.appendChild(cardContainer)
   }
 
 }
