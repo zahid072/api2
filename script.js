2268c9082f2cc3dd9decd0a74b4b099be2f6ede0
zahid072 <jahidhasan19368@gmail.com>
2024-02-29 21:54:11 +0600

    add all info in modal

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-const loadPhone = async (brands = "iphone") => {
+const loadPhone = async (brands = "iphone", isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${brands}`
   );
   let datas = await res.json();
-  displayPhone(datas.data);
+  displayPhone(datas.data, isShowAll);
 };
 loadPhone();
 
-const displayPhone = (datas) => {
+const displayPhone = (datas, isShowAll) => {
   const container = document.getElementById("allCard");
-
   const showAllContainer = document.getElementById("showAllContainer");
-  if (datas.length > 12) {
-    showAllContainer.classList.remove("hidden");
-  } else {
-    showAllContainer.classList.add("hidden");
+  if (!isShowAll) {
+    datas = datas.slice(0, 12);
+    container.innerHTML = "";
   }
-  datas = datas.slice(0, 12);
-  container.innerHTML = "";
+  if (datas.length > 12) {
+    showAllContainer.classList.add("hidden");
+  } else {
+    showAllContainer.classList.remove("hidden");
+  }
   for (let data of datas) {
     let cardContainer = document.createElement("div");
     cardContainer.classList.add("card", "w-96", "bg-base-100", "shadow-xl");
     cardContainer.innerHTML = `
          <!-- card -->
-         
             <figure class="px-10 pt-10">
               <img src=${data.image} alt="Shoes" class="rounded-xl" />
             </figure>
@@ -32,7 +32,7 @@ const displayPhone = (datas) => {
               <p>There are many variations of passages of available, but the majority have suffered</p>
               <h1 class="font-bold">$999</h1>
               <div class="card-actions">
-                <button onclick="seeDetails.showModal()" class="btn btn-primary">See Details</button>
+                <button onclick="showModal('${data.slug}')" class="btn btn-primary">See Details</button>
               </div>
             </div>
           
@@ -43,19 +43,61 @@ const displayPhone = (datas) => {
   handleSearchHandleLoader(false);
 };
 
-const searchHandle = () => {
+const searchHandle = (isShowAll) => {
   handleSearchHandleLoader(true);
   const searchFiled = document.getElementById("searchFiled");
-  loadPhone(searchFiled.value);
-  searchFiled.value = "";
+  loadPhone(searchFiled.value, isShowAll);
 };
 
 const handleSearchHandleLoader = (loader) => {
   const loaderHandler = document.getElementById("loaderHandle");
-
   if (loader === true) {
     loaderHandler.classList.remove("hidden");
   } else {
     loaderHandler.classList.add("hidden");
   }
+};
+
+const showAll = () => {
+  searchHandle(true);
+};
+
+const showModal = async (id) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/phone/${id}`
+  );
+  let datas = await res.json();
+  console.log(datas);
+  const modalContainer = document.getElementById("modalContainer");
+  modalContainer.innerHTML = "";
+  const modal = document.createElement("div");
+  modal.classList.add(
+    "fixed",
+    "bg-[#16151579]",
+    "w-full",
+    "h-screen",
+    "top-0",
+    "closeModal"
+  );
+  modal.innerHTML = `
+       <div class="mx-auto p-10 md:w-[700px] rounded-xl bg-slate-200 space-y-4 mt-20">
+        <img src="${datas.data.image}" alt="">
+        <h1 class="font-bold">Name: ${datas.data.name}</h1>
+        <p class="font-semibold">Brand: ${datas.data.brand}</p>
+        <p>Storage: ${datas.data.mainFeatures.storage}</p>
+        <p>Display: ${datas.data.mainFeatures.displaySize}</p>
+        <p>ChipSet: ${datas.data.mainFeatures.chipSet}</p>
+        <p>Memory: ${datas.data.mainFeatures.memory}</p>
+        <p>ReleaseDate: ${datas.data.releaseDate}</p>
+        <button onclick="closeModal()" class="btn btn-error">close</button>
+        </div>
+  `;
+  modalContainer.appendChild(modal);
+  let modalBox = document.querySelector(".closeModal");
+  modalBox.classList.remove("hidden");
+};
+
+const closeModal = () => {
+  const modalBox = document.querySelector(".closeModal");
+  modalBox.classList.add("hidden");
 };
